e2d98bcafadf9d30ace8f52c0512690f052f07f4
Cedric Finance <pticedric@gmail.com>
2012-06-22 13:40:03 +0200

    Added few metadata

--- xwiki/metadata.rb
+++ xwiki/metadata.rb
@@ -4,3 +4,9 @@ license          "All rights reserved"
 description      "Installs/Configures xwiki"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+recipe           "xwiki", "Install the xwiki application"
+
+supports 'ubuntu'
+
+depends 'tomcat'
+depends 'zip'
